442fe50aea2ebd5a3a14beb00f6ed0f52c6b6a41
Ramses van Zon <rzon@scinet.utoronto.ca>
2016-05-12 12:15:05 -0400

    Added license information to source file headers

--- mrtest.sh
+++ mrtest.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 #
-# mrtest.sh - tests resource management
+# mrtest.sh - tests resource management with mresource
 #
-# It first creates a file with 5 resource keys, i.e., R1, R2, R3, R4, and R5.
+# Copyright (c)  2013-2016  Ramses van Zon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions;
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#
+# -------------------------
+# Explanation of the script
+# -------------------------
+#
+# This script first creates a file with 5 resource keys, i.e., R1, R2,
+# R3, R4, and R5.
 #
 # It then loops over N numbers, grabbing and locking a resource from
 # this file, and lauching a background process that will eventually
